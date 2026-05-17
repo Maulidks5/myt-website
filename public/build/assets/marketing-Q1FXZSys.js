6b4b109b94bcd10549e2268b6bb8425437cf1333
Maulidks5 <maulid5salum@gmail.com>
2026-05-17 09:11:26 +0300

    Fix admin login CSRF handling

---
+++ public/build/assets/marketing-Q1FXZSys.js
@@ -0,0 +1,12 @@
+import{G as o,b as r,C as s,a as t,P as c,c as i,S as m}from"./share-2-DXWf8pN-.js";import{c as e}from"./app-dCuZriK-.js";import{E as n}from"./eye-0AND1UgW.js";import{L as p,T as l}from"./target-CdmAqBJ0.js";import{M as d}from"./map-pin-BeDk9A43.js";import{S as y}from"./shield-check-meeFG_gY.js";import{U as C}from"./users-BXXak19f.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=e("Award",[["path",{d:"m15.477 12.89 1.515 8.526a.5.5 0 0 1-.81.47l-3.58-2.687a1 1 0 0 0-1.197 0l-3.586 2.686a.5.5 0 0 1-.81-.469l1.514-8.526",key:"1yiouv"}],["circle",{cx:"12",cy:"8",r:"6",key:"1vp47v"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=e("Heart",[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}]]),P=["Globe","Code2","Palette","Camera","Share2","Server"],S={Award:f,Camera:r,Clock3:s,Code2:t,Eye:n,Globe:o,Heart:h,Lightbulb:p,MapPin:d,Palette:c,Server:i,Share2:m,ShieldCheck:y,Target:l,Users:C},u=a=>S[a??""]??o,G=a=>(a??[]).join(`
+`);export{G as a,u as g,P as i};
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,7 @@
 import { createInertiaApp } from "@inertiajs/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
+import "./bootstrap";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
